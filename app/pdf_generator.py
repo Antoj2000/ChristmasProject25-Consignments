eb3fbddf0d912aa30953d9d1b04ad8f93e9f414f
Antoj2000 <g00385306@atu.ie>
2025-11-16 00:23:56 +0000

    Label Graphic Changes

--- app/pdf_generator.py
+++ app/pdf_generator.py
@@ -1,5 +1,6 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A6
+from reportlab.lib import colors
 import os
 
 
@@ -14,19 +15,27 @@ def generate_label_pdf(consignment):
     # Create Blank PDF
     c = canvas.Canvas(filepath, pagesize=A6)
 
+    #Box Dims
+    box_x = 15
+    box_y = 150
+    box_width = 170
+    box_height = 100
     # Label Layout
-    c.setFont("Helvetica-Bold", 12)
-    c.drawString(20,260, f"Consignment ID: {consignment.id}")
+    c.setLineWidth(1)
+    c.setStrokeColor(colors.black)
+    c.rect(box_x, box_y, box_width, box_height, stroke=1, fill=0)
 
     c.setFont("Helvetica", 10)
-    c.drawString(20, 230, f"Name: {consignment.name}")
+    c.drawString(20, 230, f"{consignment.name}")
     c.drawString(20, 210, f"{consignment.addressline1}")
     if consignment.addressline2:
         c.drawString(20, 195, f"{consignment.addressline2}")
     c.drawString(20, 180, f"{consignment.addressline3}")
     c.drawString(20, 165, f"{consignment.addressline4}")
 
-    c.drawString(20, 140, f"Weight: {consignment.weight}kg")
+    c.drawString(20, 130, f"Consignment ID: {consignment.id}")
+
+    c.drawString(20, 100, f"Weight: {consignment.weight}kg")
 
     c.save()
 
